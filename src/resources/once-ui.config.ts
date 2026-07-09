d9c48a0c4f6f53857037a38df058e8cb04f3da9a
Tex <cruz.tyrone2003@gmail.com>
2026-07-09 14:38:21 +0800

    Update once-ui.config.ts

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -14,7 +14,7 @@ import {
 import { home, person, social } from "./content";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://demo.magic-portfolio.com";
+const baseURL: string = "https://tex-cruz.vercel.app";
 
 const routes: RoutesConfig = {
   "/": true,
